7d4d8afbf73febb7545ae862d2bcb384fff84efc
minus <minus@mnus.de>
2015-11-12 23:30:40 +0100

    improved on_command, added _bg
    await_command/on_command now take *args: the first len(args) must match
    _bg allows running tasks in background while logging exceptions
    IrcMessage does not have .command anymore, use .args[0]

--- bot/bot.py
+++ bot/bot.py
@@ -5,9 +5,8 @@ import asyncio
 from collections import namedtuple
 import logging
 import re
-from typing import List, Callable, Union
-
-MessageHandler = namedtuple("MessageHandler", ("matcher", "handler"))
+from typing import List, Callable, Union, Sequence
+from types import coroutine
 
 
 class LoggerMetaClass(type):
@@ -79,7 +78,7 @@ class Client(metaclass=LoggerMetaClass):
         self._on_message_handlers = []
         self._users = {}
         self._channels = {}
-        self._on_command_handlers = {}
+        self._on_command_handlers = []
         self._channel_types = ""
         self._connected = False
 
@@ -89,6 +88,8 @@ class Client(metaclass=LoggerMetaClass):
             return fn
 
         return decorator
+
+    MessageHandler = namedtuple("MessageHandler", ("matcher", "handler"))
 
     def on_message(self, message: Union[str, RegEx]=None, channel: Union[str, RegEx]=None,
                    sender: Union[str, RegEx]=None, matcher: Callable[[Message], None]=None
@@ -151,32 +152,33 @@ class Client(metaclass=LoggerMetaClass):
                 return all(m(msg) for m in matchers)
 
         def decorator(fn: Callable[[Message], None]) -> Callable[[Message], None]:
-            handler = MessageHandler(matcher, fn)
+            handler = self.MessageHandler(matcher, fn)
             self._on_message_handlers.append(handler)
             return fn
 
         return decorator
 
-    IrcMessage = namedtuple("IrcMessage", ("prefix", "command", "args", "rest"))
+    IrcMessage = namedtuple("IrcMessage", ("prefix", "args", "rest"))
 
-    def on_command(self, command: str) -> Callable[[Callable], Callable]:
+    CommandHandler = namedtuple("CommandHandler", ("args", "rest", "handler"))
+
+    def on_command(self, *args: Sequence[str], rest: str=None) -> Callable[[Callable], Callable]:
         def decorator(fn: Callable[[self.IrcMessage], None]):
-            self._on_command_handlers.setdefault(command, []).append(fn)
+            self._on_command_handlers.append(self.CommandHandler(args, rest, fn))
             return fn
 
         return decorator
 
-    def remove_command_handler(self, command: str, handler: Callable[[IrcMessage], None]) -> None:
-        handler_list = self._on_command_handlers[command]
-        handler_list.remove(handler)
-        if not handler_list:
-            del self._on_command_handlers[command]
+    def remove_command_handler(self, handler: Callable[[IrcMessage], None]) -> None:
+        for ch in self._on_command_handlers:
+            if ch.handler == handler:
+                self._on_command_handlers.remove(ch)
 
-    async def await_command(self, command: str) -> IrcMessage:
+    async def await_command(self, *args: Sequence[str], rest: str=None) -> IrcMessage:
         fut = asyncio.Future()
-        @self.on_command(command)
+        @self.on_command(*args, rest=rest)
         async def handler(msg):
-            self.remove_command_handler(command, handler)
+            self.remove_command_handler(handler)
             fut.set_result(msg)
         return await fut
 
@@ -191,8 +193,7 @@ class Client(metaclass=LoggerMetaClass):
             args = msg.split()
         else:
             args = msg.split()
-        command = args.pop(0)
-        return self.IrcMessage(prefix, command, args, rest)
+        return self.IrcMessage(prefix, tuple(args), rest)
 
     def buildmsg(self, *args: List[str], prefix: str=None, rest: str=None) -> str:
         msg = ""
@@ -244,17 +245,19 @@ class Client(metaclass=LoggerMetaClass):
             if not msg:
                 continue
 
-            if msg.command in self._on_command_handlers:
-                for handler in self._on_command_handlers[msg.command]:
-                    asyncio.ensure_future(handler(msg))
+            for ch in self._on_command_handlers:
+                args = msg.args[:len(ch.args)]
+                self._log.debug("Evaluating command handler {} with input {} rest={}".format(ch, args, msg.rest))
+                if ch.args == args and (not ch.rest or ch.rest == msg.rest):
+                    self._bg(ch.handler(msg))
 
             if not self._connected:
                 continue
 
-            if msg.command in ("PRIVMSG", "NOTICE"):
+            if msg.args[0] in ("PRIVMSG", "NOTICE"):
                 sender = self._resolve_sender(msg.prefix)
                 recipient = self._resolve_recipient(msg.args[0])
-                message = Message(sender, recipient, msg.rest, (msg.command == "NOTICE"))
+                message = Message(sender, recipient, msg.rest, (msg.args[0] == "NOTICE"))
                 await self._handle_on_message(message)
                 continue
 
@@ -262,8 +265,18 @@ class Client(metaclass=LoggerMetaClass):
 
         self._log.info("Connection closed, exiting")
 
+    def _bg(self, coro: coroutine):
+        """Run coro in background, log errors"""
+        async def runner():
+            try:
+                await coro
+            except:
+                self._log.exception("async: Coroutine raised exception")
+                raise  # reraise for the heck of it
+        asyncio.ensure_future(runner())
+
     async def _handle_special(self, msg: IrcMessage) -> bool:
-        if msg.command == "PING":
+        if msg.args[0] == "PING":
             await self.send("PONG", rest=msg.rest)
             return True
         return False
@@ -281,7 +294,7 @@ class Client(metaclass=LoggerMetaClass):
         async def feature_list(msg):
             for feature in filter(lambda arg: arg.startswith("CHANTYPES="), msg.args):
                 self._channel_types = feature.partition("=")[2]
-                self.remove_command_handler("005", feature_list)
+                self.remove_command_handler(feature_list)
 
         self._log.debug("Waiting for the end of the MOTD")
         await self.await_command("376")  # End of MOTD
@@ -317,7 +330,9 @@ class Client(metaclass=LoggerMetaClass):
 
     async def join(self, channel: str) -> Channel:
         await self.send("JOIN", channel)
-        #await self.await_command("JOIN", channel)
+        self._log.debug("Joining channel {}".format(channel))
+        await self.await_command("JOIN", rest=channel)
+        self._log.info("Joined channel {}".format(channel))
 
     async def quit(self, reason: str=None) -> Channel:
         await self.send("QUIT", rest=reason)
